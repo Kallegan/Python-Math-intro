886d2dc40a0b3d502f6e5cc70ebb73b7da6a5e9b
Kallegan <90606627+Kallegan@users.noreply.github.com>
2022-11-08 00:24:41 +0100

    basic vector math, basic python math libs

--- math.py
+++ math.py
@@ -1,18 +1,21 @@
 import numpy as np
+import pyrr
+import math
 
-v = np.array([
-    [1],
-    [2]
-], dtype = np.float32)
+v = np.array([1, 2], dtype = np.float32)
 
 u = np.array([
     [1],
     [-1]
 ], dtype = np.float32)
 
+norm = pyrr.vector.length(v)
+norm2 = math.sqrt(v[0]**2 + v[1]**2)
+print(norm)
+print(norm2)
+print(math.sqrt(5))
 
-w = v - u
-
-
-
-print(w)
+v_hat = v / norm
+print(v_hat)
+v_hat_norm = pyrr.vector.length(v_hat)
+print(v_hat_norm)
